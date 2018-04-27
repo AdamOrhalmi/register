7fd7f1085047ac9aecb22372dacf63c6373edfe2
Adam Orhalmi <adam.orhalmi@fpt.sk>
2018-04-27 15:02:23 +0200

    upload

---
+++ test/ArrayRegisterTest.java
@@ -0,0 +1,26 @@
+package register;
+
+import static org.junit.Assert.*;
+
+public class ArrayRegisterTest {
+
+    @org.junit.Test
+    public void getPerson() {
+    }
+
+    @org.junit.Test
+    public void addPerson() {
+    }
+
+    @org.junit.Test
+    public void findPersonByName() {
+    }
+
+    @org.junit.Test
+    public void findPersonByPhoneNumber() {
+    }
+
+    @org.junit.Test
+    public void removePerson() {
+    }
+}
